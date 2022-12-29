0cb63202aec8f550e08143037b05d427e165fc46
Christian <c.espinoza.code@gmail.com>
2022-12-29 10:54:46 -0500

    sequelize configured

--- src/libs/sequelize.ts
+++ src/libs/sequelize.ts
@@ -1,5 +1,6 @@
 import {Sequelize} from 'sequelize';
 import { config } from '../config/config';
+import { setupModels } from '../database/models';
 
 /* Configuración para usar Sequelize ORM */
 const USER = encodeURIComponent(config.dbUser as string)
@@ -10,3 +11,9 @@ export const sequelize = new Sequelize(URI, {
   dialect: 'postgres',
   logging: console.log
 })
+
+// Configuración de los modelos para las tablas de la base de datos
+setupModels(sequelize);
+
+// Creación de las tablas
+sequelize.sync();
