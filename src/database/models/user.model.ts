16c823b38b87e6ddca9dfc7a91db9887202ac547
Christian <c.espinoza.code@gmail.com>
2022-12-29 10:54:09 -0500

    user model defined

---
+++ src/database/models/user.model.ts
@@ -0,0 +1,44 @@
+import {Model, DataTypes, Sequelize} from 'sequelize';
+// import { sequelize } from '../../libs/sequelize';
+
+export const USER_TABLE = 'users';
+
+// User Schema
+export const UserSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    unique: true
+  },
+  password: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'create_at',
+    defaultValue: DataTypes.NOW
+  }
+}
+
+export class User extends Model {
+  static assocciate() {
+    // 
+  }
+
+  static config(sequelize: Sequelize) {
+    return {
+      sequelize,
+      tableName: USER_TABLE,
+      modelName: 'User',
+      timestamps: false
+    }
+  }
+}
