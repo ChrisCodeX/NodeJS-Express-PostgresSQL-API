0866178ee4850cfae0864f1bc150f111fa37ade8
Christian <c.espinoza.code@gmail.com>
2022-12-31 10:51:29 -0500

    find and findOne methods created for customer.service

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize";
 import { UserSchema, User } from "./user.model";
+import { CustomerSchema, Customer } from "./customer.model";
 
 export function setupModels(sequelize: Sequelize) {
   User.init(UserSchema, User.config(sequelize))
+  Customer.init(CustomerSchema, Customer.config(sequelize))
 }
--- src/services/customer.service.ts
+++ src/services/customer.service.ts
@@ -1,16 +1,34 @@
+import { Sequelize } from "sequelize"
+import { sequelize } from "../libs/sequelize"
 
 class customerService {
   constructor() {
 
   }
 
+  /* Find all customers */
   public async find() {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       try {
-        
+        const rta = await sequelize.models.Customer.findAll()
+        resolve(rta)
       } catch (error) {
         reject(error)
       }
     })
   }
+
+  /* Find just one customer */
+  public async findOne(id:string) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const customer = await sequelize.models.Customer.findByPk(id)
+        resolve(customer)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  /* Create a customer */
 }
