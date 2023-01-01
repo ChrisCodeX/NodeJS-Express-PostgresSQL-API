9a4527016a73c2b5397aa4891d3716095054c227
Christian <c.espinoza.code@gmail.com>
2023-01-01 14:53:45 -0500

    userId property added in customer.schema

--- src/schemas/customer.schema.ts
+++ src/schemas/customer.schema.ts
@@ -4,6 +4,7 @@ const id = Joi.number().integer();
 const name = Joi.string().min(3).max(30);
 const lastName = Joi.string();
 const phone = Joi.string();
+const userId = Joi.number().integer();
 
 export const getCustomerSchema = Joi.object({
   id: id.required(),
@@ -12,11 +13,13 @@ export const getCustomerSchema = Joi.object({
 export const createCustomerSchema = Joi.object({
   name: name.required(),
   lastName: lastName.required(),
-  phone: phone.required()
+  phone: phone.required(),
+  userId: userId.required()
 })
 
 export const updateCustomerSchema = Joi.object({
   name,
   lastName,
-  phone
+  phone,
+  userId
 })
