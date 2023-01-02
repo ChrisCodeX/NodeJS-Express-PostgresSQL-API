93a35e002b0dd265ea50915f36ce5e26346e9bc3
Christian <c.espinoza.code@gmail.com>
2023-01-01 21:54:06 -0500

    customer getMethod created

--- src/routes/customer.router.ts
+++ src/routes/customer.router.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { CustomerService } from '../services/customer.service';
+import { validatorHandler } from '../middleware/validator.handler';
+import { createCustomerSchema, getCustomerSchema, updateCustomerSchema } from '../schemas/customer.schema';
 
 /* Router */
 const router = express.Router();
@@ -8,4 +10,15 @@ const router = express.Router();
 const customerService = new CustomerService()
 
 /* Get Methods */
-
+router.get('/',
+  async (req, res, next)=>{
+    return new Promise<unknown>((resolve, reject) => {
+      try {
+        const customers = customerService.find()
+        res.status(200).json(customers)
+        resolve(customers)
+      } catch (error) {
+        reject(error)
+      }
+    })
+})
