d28806f063cf0857ac0ed618fe9044ebce13c5b8
Christian <c.espinoza.code@gmail.com>
2023-01-01 21:34:57 -0500

    getCustomerSchema property validated with joi

--- src/schemas/customer.schema.ts
+++ src/schemas/customer.schema.ts
@@ -4,3 +4,8 @@ const id = Joi.number().integer();
 const name = Joi.string().min(3).max(30);
 const lastName = Joi.string();
 const phone = Joi.string();
+
+const getCustomerSchema = Joi.object({
+  id: id.required(),
+})
+
